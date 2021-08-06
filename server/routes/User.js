7f4a60834b8b169fd492b85583d85269265d064a
Jamin <jamin.cheung@macys.com>
2021-08-06 18:42:45 -0400

    Add user/:id route

--- server/routes/User.js
+++ server/routes/User.js
@@ -8,4 +8,13 @@ router.get('/users', async (req, res) => {
   res.json(users)
 })
 
+router.get('/user/:id', async (req, res) => {
+  const { id } = req.params
+  const user = await User.findAll({
+    where: { id, }
+  })
+
+  res.json(user)
+})
+
 module.exports = router;
