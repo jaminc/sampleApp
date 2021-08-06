2f6f4f6dc4df51ec14aef1549ef21ae8612ada6e
Jamin <jamin.cheung@macys.com>
2021-08-06 14:27:28 -0400

    Add bodyParser middleware

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const port = 3000
 
 const { Sequelize } = require('sequelize')
+const app = express()
 
 // const sequelize = new Sequelize({
 //   dialect: 'sqlite',
   // storage:
 // })
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true,
+}))
 
-const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
